db4a65715d8a4061aaff3e098950c3b0b548f1fe
Sara-Morales <moralessaramelissa@gmail.com>
2023-10-03 20:14:13 -0500

    Valor total carrito de compra

--- usuario/app.js
+++ usuario/app.js
@@ -47,46 +47,51 @@ function visualizarProductos() {
             contenedor.innerHTML += `<div><img src="${productos[i].urlImagen}"><div class="informacion"><p>${productos[i].nombre}</p><p class="precio">$${productos[i].valor}</p><button onclick=comprar(${i})>Comprar</button></div></div>`
         }
         else {
-            contenedor.innerHTML += `<div><img src="${productos[i].urlImagen}"><div class="informacion"><p>${productos[i].nombre}</p><p class="precio">$${productos[i].valor}</p><p class="soldOut">Sold Out</p></div></div>`
+            contenedor.innerHTML += `<div><img src="${productos[i].urlImagen}"><div class="informacion"><p>${productos[i].nombre}</p><p class="precio">$${productos[i].valor}</p><p class="Agotado">Agotado
+            </p></div></div>`
         }
     }
 }
 
 function comprar(indice) {
-    lista.push({ nombre: productos[indice].nombre, precio: productos[indice].valor });
+    lista.push({ nombre: productos[indice].nombre, precio: productos[indice].valor })
 
-    let van = true;
-    for (let i = 0; i < productos.length && van; i++) {
-        if (productos[i].nombre === productos[indice].nombre) {
-            productos[i].existencia -= 1;
-            if (productos[i].existencia === 0) {
-                visualizarProductos();
+    let van = true
+    let i = 0
+    while (van == true) {
+        if (productos[i].nombre == productos[indice].nombre) {
+            productos[i].existencia -= 1
+            if (productos[i].existencia == 0) {
+                visualizarProductos()
             }
-            van = false;
+            van = false
         }
+        guardarAlmacenamientoLocal("productos", productos)
+        i += 1
     }
-
-    guardarAlmacenamientoLocal('productos', productos);
-
-    numero.innerHTML = lista.length;
-    numero.classList.add('diseñoNumero');
-
-    mostrarElementosLista(); // Añadido: Mostrar elementos en el carrito después de comprar.
-    return lista;
+    numero.innerHTML = lista.length
+    numero.classList.add("diseñoNumero")
+    return lista
 }
 
-function mostrarElementosLista() {
-    productosCompra.innerHTML = '';
+carrito.addEventListener("click", function(){
+    body.style.overflow = "hidden"
+    contenedorCompra.classList.remove('none')
+    contenedorCompra.classList.add('contenedorCompra')
+    informacionCompra.classList.add('informacionCompra')
+    mostrarElemtrosLista()
+})
+
+function mostrarElemtrosLista() {
+    productosCompra.innerHTML = "";
     valortotal = 0;
     for (let i = 0; i < lista.length; i++){
         productosCompra.innerHTML += `<div><div class="img"><button onclick=eliminar(${i}) class="botonTrash"><img src="/img/trash.png"></button><p>${lista[i].nombre}</p></div><p> $${lista[i].precio}</p></div>`;
         valortotal += parseFloat(lista[i].precio);
     }
 
-    total.innerHTML = `<p>Valor Total</p> <p><span>$${valortotal.toFixed(0)}</span></p>`;
+    total.innerHTML = `<p>Valor Total</p> <p><span>$${valortotal.toFixed(3)}</span></p>`;
 }
-
-
 
 function eliminar(indice){
     let van = true
